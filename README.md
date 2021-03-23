01a481a309ca51458a3e9c92769324c2075ffc4a
Nils Hartmann <nils1hartmann@gmail.com>
2021-03-23 21:13:14 +0100

    Removed all themes except default for now. Also fixed many linting issues that didn't show before

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-Pretty much a copy of [https://github.com/darthmorf/Terraria-Logo-Maker-2](https://github.com/darthmorf/Terraria-Logo-Maker-2) but as a node module.
+## Description
+This is a NodeJs Module that generates Terraria logos from text. I used the basic code from [https://github.com/darthmorf/Terraria-Logo-Maker-2](https://github.com/darthmorf/Terraria-Logo-Maker-2) but changed it a bit to make it a NodeJs Module. I also wrote it in typescript.
+
+## Example Use:
+
+For javascript:
+```javascript
+const generator = require("terrarialogomaker");
+const fs = require("fs");
+
+generator.createLogo("Hello World", true).then((image) => {
+    fs.writeFileSync("image.png", image);
+});
+```
+
+And typescript:
+```typescript
+import { createLogo } from "terrarialogomaker";
+import { writeFileSync } from "fs";
+
+createLogo("Hello World", true).then((image) => {
+    writeFileSync("image.png", image);
+});
+```
--- index.ts
+++ index.ts
@@ -1,19 +1,19 @@
 import { createCanvas, loadImage,  Image } from 'canvas';
 import * as path from "path";
 
-var canvas = createCanvas(0, 95);
-var ctx = canvas.getContext("2d");
+const canvas = createCanvas(0, 95);
+const ctx = canvas.getContext("2d");
 
-let imageDir = path.join(__dirname, "../assets/chars");
-let theme: String;
+const imageDir = path.join(__dirname, "../assets/chars");
+let theme: string;
 let treeImg: Image;
 let startImg: Image;
 let endImg: Image;
 
 
-loadTheme('overworld-extended');
+loadTheme('overworld');
 
-function loadTheme (newTheme: String){
+function loadTheme (newTheme: string){
     theme = newTheme;
     treeImg = new Image();
     treeImg.src = `${imageDir}/${theme}/tree.png`;
@@ -25,7 +25,7 @@ function loadTheme (newTheme: String){
     endImg.src = `${imageDir}/${theme}/end.png`;
 }
 
-function displayImage (images:Image[], drawTree:Boolean) {
+function displayImage (images:Image[], drawTree:boolean) {
     if (drawTree){
         images = [treeImg, startImg].concat(images, [endImg]);
     }
@@ -35,35 +35,34 @@ function displayImage (images:Image[], drawTree:Boolean) {
 
     ctx.clearRect(0, 0, canvas.width, canvas.height); 
 
-    var imageWidth = 0; 
+    let imageWidth = 0; 
     for (let i = 0; i < images.length; i++) {
         imageWidth += images[i].width;
     }
     canvas.width = imageWidth;
 
-    var x = 0;
-    var y = 0;
+    let x = 0;
     for (let i = 0; i < images.length; i++) {
-        ctx.drawImage(images[i], x, y);
+        ctx.drawImage(images[i], x, 0);
         x += images[i].width;
     }    
 }
 
-async function stringToImage (text: String, charImages:Image[]=[]): Promise<Image[]> {
+async function stringToImage (text: string, charImages:Image[]=[]): Promise<Image[]> {
     if (text.length == 0) {
         return charImages;
     }
-    var asciiVal = text[0].charCodeAt(0);    
-    var imagePath = `${imageDir}/${theme}/${asciiVal}.png`;
+    const asciiVal = text[0].charCodeAt(0);    
+    const imagePath = `${imageDir}/${theme}/${asciiVal}.png`;
     const image = await loadImage(imagePath);
     charImages.push(image);
     return await stringToImage(text.slice(1, text.length), charImages);
 }
 
-export async function createLogo(text: String, drawTree: Boolean): Promise<Buffer> {
+export async function createLogo(text: string, drawTree: boolean): Promise<Buffer> {
     const images = await stringToImage(text);
     displayImage(images, drawTree);
-    var dataURL = canvas.toDataURL('image/png');
-    var data = dataURL.replace(/^data:image\/\w+;base64,/, "");
+    const dataURL = canvas.toDataURL('image/png');
+    const data = dataURL.replace(/^data:image\/\w+;base64,/, "");
     return Buffer.from(data, 'base64');
 }
